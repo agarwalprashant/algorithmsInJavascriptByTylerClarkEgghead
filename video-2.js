33d4cd6e66ddbcebf798be5e903957d5aedecbdb
prashant agarwal <agarwalprashant355@gmail.com>
2019-04-26 17:57:32 +0530

    convert loopping function to recursive function

--- video-2.js
+++ video-2.js
@@ -1,8 +1,22 @@
-function counter(){
-    for(let n=0;n<=10;n++){
-        console.log(n);
+// function counter(){
+//     for(let n=0;n<=10;n++){
+//         console.log(n);
+//     }
+// }
+
+
+// counter();
+
+
+// Now we will convert this above counter looping function to a recursive function
+// Refactor a Loop in JavaScript to Use Recursion
+
+function counter(n){
+    console.log(n)
+    if(n === 10){
+        return;
     }
+    counter(n+1);
 }
 
-
-counter();
+counter(0);
