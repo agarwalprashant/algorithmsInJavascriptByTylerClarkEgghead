aa03d6675f2bc6be922abe5db546c2d8651620b2
prashant agarwal <agarwalprashant355@gmail.com>
2019-04-26 19:10:51 +0530

    recursive function to get sum of all el in an arry

--- video-3.js
+++ video-3.js
@@ -1,13 +1,26 @@
+// function sum(arr){
+//     let sum=0;
+//     for(let i of arr){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+
+
+// console.log(sum([1,2,3,4,5]));  
+
+
+// divide and conquer algorithms are recursive algorithms
+//its a way to think about a problem decomposition
+
 function sum(arr){
-    let sum=0;
-    for(let i of arr){
-        sum+=i;
-    }
-    return sum;
+    if(arr.length === 0) return 0;
+    arr
+    return arr[0] + sum(arr.slice(1));
 }
 
+console.log(sum([1,2,3,4,5]))
 
-console.log(sum([1,2,3,4,5]));  
 
 
 
